8e5ebe3042cbd54fef6b76e5bac3fab49bcaa220
dangrana <danmgrana@users.noreply.github.com>
2016-06-09 20:46:10 -0500

    init

---
+++ app/mailers/my_mailer.rb
@@ -0,0 +1,11 @@
+class MyMailer < ActionMailer::Base
+  default from: ENV["gmail_user_name"]
+
+  # def welcome_email(userid)
+  def welcome_email(user)
+    # @user = User.find(1)
+    @user = user
+    # @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
