a4cc7e96850ac4d33ae82f282168cda2e8de879a
dangrana <danmgrana@users.noreply.github.com>
2016-06-09 20:59:05 -0500

    9

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module Tickerpush
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # ADDED
+
+        config.action_mailer.delivery_method = :smtp
+        config.action_mailer.smtp_settings = {
+          address:              'smtp.gmail.com',
+          port:                 587,
+          # domain:               'example.com',
+          user_name:            ENV["gmail_user_name"],
+          password:             ENV["gmail_password"],
+          authentication:       'plain',
+          enable_starttls_auto: true  }
   end
 end
